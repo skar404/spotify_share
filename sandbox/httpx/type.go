10f9374e5d2e7d1f4ef93ecb226a07449f928593
skar404 <skar404@gmail.com>
2020-11-14 14:37:39 +0700

    init test http client

---
+++ sandbox/httpx/type.go
@@ -0,0 +1,22 @@
+package httpx
+
+type Response struct {
+	Code int
+	Url  string
+}
+
+type ResponseByte struct {
+	Response
+	Body []byte
+}
+
+type ResponseRaw struct {
+	Response
+	Body string
+}
+
+type ResponseJson struct {
+	Response
+	Body interface{}
+	Raw  string
+}
